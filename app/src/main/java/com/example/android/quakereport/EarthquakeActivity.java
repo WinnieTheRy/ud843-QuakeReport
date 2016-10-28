562bf2bc7c562c5b6927a80ea492288e1d596785
RyanH <haniffryan.hr@gmail.com>
2016-10-27 20:48:42 -0400

    Added emoty textview if there are no earthquakes

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -20,6 +20,8 @@ import android.content.AsyncTaskLoader;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -56,16 +58,15 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        //final ArrayList<EarthquakeData> earthquakeDatas = QueryUtils.extractEarthquakes();
-
-//        BackgroundNetworkConnection backgroundNetworkCOnnection = new BackgroundNetworkConnection();
-//        backgroundNetworkCOnnection.execute(USGS_URL);
-
         ListView listView = (ListView) findViewById(R.id.list);
-
+        //pass in a empty arraylist
         mAdapter = new EarthquakeAdapter(this, new ArrayList<EarthquakeData>());
-
         listView.setAdapter(mAdapter);
+
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+
+        //sets a text display if there is not list items to update
+        listView.setEmptyView(mEmptyStateTextView);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -78,14 +79,26 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
             }
         });
 
-        getLoaderManager().initLoader(0, null, this);
+        //Checking to see if we have network connection
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
 
-        //sets a text display if there is not list items to update
-        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
-        listView.setEmptyView(mEmptyStateTextView);
+        //if we are connected to internet run this
+        if (networkInfo != null && networkInfo.isConnected()) {
 
-        mSpinnerProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
-        mSpinnerProgressBar.setVisibility(View.VISIBLE);
+            //sets the progressbar to be visible
+            mSpinnerProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+            mSpinnerProgressBar.setVisibility(View.VISIBLE);
+            getLoaderManager().initLoader(0, null, this);
+        } else {
+            //set the spinner loader to invisible so error message will be visible
+            mSpinnerProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+            mSpinnerProgressBar.setVisibility(View.GONE);
+
+            //setting text to "No network connection"
+            mEmptyStateTextView.setText(R.string.no_network);
+        }
+
 
         Log.v(LOG_TAG, "TEST: OnCreate methode");
 
@@ -96,11 +109,11 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
 
         Log.v(LOG_TAG, "TEST: onCreateLoader methode");
 
-        //sets the progressbar to be visible
-        //mSpinnerProgressBar.setVisibility(View.VISIBLE);
+//        //sets the progressbar to be visible
+//        mSpinnerProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+//        mSpinnerProgressBar.setVisibility(View.VISIBLE);
 
         return new EarthquakeLoader(this, USGS_URL);
-
 
 
     }
@@ -114,9 +127,11 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
             //mAdapter.addAll(data);
         }
 
-        //set progress bar to invisible when finshed loading data
-        mSpinnerProgressBar.setVisibility(View.INVISIBLE);
+//        //set progress bar to invisible when finshed loading data
+//        mSpinnerProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+//        mSpinnerProgressBar.setVisibility(View.GONE);
 
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
         mEmptyStateTextView.setText(R.string.no_earthquake);
 
         Log.v(LOG_TAG, "TEST: onLoadFinished methode");
